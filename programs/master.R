fac2234847ca77d1c60639cc6ee1c7204ea6be04
Lars Vilhuber <lars@vilhuber.com>
2022-01-16 20:45:49 -0500

    Bugfix

--- programs/master.R
+++ programs/master.R
@@ -2,7 +2,7 @@
 
 source("config.R",echo=TRUE)
 
-source(programs,"01_dataclean.R",echo=TRUE)
-source(programs,"02_table1.R",echo=TRUE)
+source(file.path(programs,"01_dataclean.R"),echo=TRUE)
+source(file.path(programs,"02_table1.R"),echo=TRUE)
 
 
